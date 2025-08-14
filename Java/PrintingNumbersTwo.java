3ee86b4c97e1da9ac23f0491b1fbd1a8682c3daa
Cazaks <calebezak@gmail.com>
2025-08-14 05:59:52 +0100

    Testing commit

---
+++ Java/PrintingNumbersTwo.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+
+public class PrintingNumbersTwo{
+public static void main (String [] args){
+
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter a number between 0 and 9:");
+	int userInput = scan.nextInt();
+
+	if(userInput == 0){
+		System.out.println("Zero");	
+	}
+
+	else
+	if(userInput == 1){
+		System.out.println("One");	
+	}
+
+	else
+	if(userInput == 2){
+		System.out.println("Two");	
+	}
+
+	else
+	if(userInput == 3){
+		System.out.println("Three");	
+	}
+
+	else
+	if(userInput == 4){
+		System.out.println("Four");	
+	}
+
+	else
+	if(userInput == 5){
+		System.out.println("Five");	
+	}
+
+	else
+	if(userInput == 6){
+		System.out.println("Six");	
+	}
+
+	else
+	if(userInput == 7){
+		System.out.println("Seven");	
+	}
+
+	else
+	if(userInput == 8){
+		System.out.println("Eight");	
+	}
+
+	else
+	if(userInput == 9){
+		System.out.println("Nine");	
+	}
+
+	else{
+		System.out.println("Invalid input. Enter a valid number between 0 - 9");
+
+		System.out.println("Enter a number between 0 and 9:");
+		userInput = scan.nextInt();
+
+		if(userInput == 0){
+			System.out.println("Zero");	
+		}
+
+		else
+		if(userInput == 1){
+			System.out.println("One");	
+		}
+
+		else
+		if(userInput == 2){
+			System.out.println("Two");	
+		}
+
+		else
+		if(userInput == 3){
+			System.out.println("Three");	
+		}
+
+		else
+		if(userInput == 4){
+			System.out.println("Four");	
+		}
+
+		else
+		if(userInput == 5){
+			System.out.println("Five");	
+		}
+
+		else
+		if(userInput == 6){
+			System.out.println("Six");	
+		}
+
+		else
+		if(userInput == 7){
+			System.out.println("Seven");	
+		}
+
+		else
+		if(userInput == 8){
+			System.out.println("Eight");	
+		}
+
+		else
+		if(userInput == 9){
+			System.out.println("Nine");	
+		}
+
+	}
+	
+}
+}
---
+++ JavaScript/PrintingNumbers.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+const number = prompt("Enter a number between 1 - 10 ");
+
+if (number == 0);
+	console.log("Zero")
