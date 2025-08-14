40cbf0236df49e369d397a2fee99ce4db2f46c3b
Cazaks <calebezak@gmail.com>
2025-08-14 07:38:57 +0100

    Updating with a loop

---
+++ Java/PrintingNumbersLoop.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class PrintingNumbersLoop{
+public static void main(String[] args){
+
+	Scanner scan = new Scanner(System.in);
+
+	int userInput = 1;
+
+	while (true){
+	
+		System.out.println("Enter a number between 0 - 9");
+		userInput = scan.nextInt();
+
+		if(userInput >= 0 && userInput <= 9){
+			break;
+		}
+		else{
+			System.out.println("Invalid in. Enter a number between 0 - 9");
+		}
+	}
+
+	if(userInput == 0){
+		System.out.println("Zero");	
+	}
+
+	else
+	if(userInput == 1){
+		System.out.println("One");	
+	}
+
+	else
+	if(userInput == 2){
+		System.out.println("Two");	
+	}
+
+	else
+	if(userInput == 3){
+		System.out.println("Three");	
+	}
+
+	else
+	if(userInput == 4){
+		System.out.println("Four");	
+	}
+
+	else
+	if(userInput == 5){
+		System.out.println("Five");	
+	}
+
+	else
+	if(userInput == 6){
+		System.out.println("Six");	
+	}
+
+	else
+	if(userInput == 7){
+		System.out.println("Seven");	
+	}
+
+	else
+	if(userInput == 8){
+		System.out.println("Eight");	
+	}
+
+	else
+	if(userInput == 9){
+		System.out.println("Nine");	
+	}
+
+}
+}
