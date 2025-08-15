cfe9a6030a522b1c937027e35739d836831530fe
Cazaks <calebezak@gmail.com>
2025-08-15 03:29:13 +0100

    Correction of indexes to index

--- ArraysIndex.java
+++ ArraysIndex.java
@@ -5,7 +5,7 @@ public static void main(String[]args){
 	int[] values = {45, 2, 55, 1, 0, 7, 88, 23};
 
 	for(int index = 0; index < values.length; index++){
-		System.out.println("The element of indexes " + index + " is: " + values[index]);
+		System.out.println("The element of indexe " + index + " is: " + values[index]);
 	}
 }
 }
