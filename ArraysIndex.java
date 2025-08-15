353adf74e359e8d139c0bd02357eea82131b6bb3
Cazaks <calebezak@gmail.com>
2025-08-15 03:17:42 +0100

    Array index exercise

---
+++ ArraysIndex.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ArraysIndex{
+public static void main(String[]args){
+
+	int[] values = {45, 2, 55, 1, 0, 7, 88, 23};
+
+	for(int index = 0; index < values.length; index++){
+		System.out.println("The element of indexes " + index + " is: " + values[index]);
+	}
+}
+}
