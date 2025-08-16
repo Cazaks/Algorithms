b0cb249148cb22eb8f825e99c9517b0747944be1
Cazaks <calebezak@gmail.com>
2025-08-16 03:00:26 +0100

    uploading

---
+++ Java/Increament.java
@@ -0,0 +1,13 @@
+public class Increament{
+	public static void main(String[]args){
+
+		int a = 10;
+		int b = a++;
+		int c = ++a;
+
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+
+	}
+}
