ed4e3adf2aaa2bfba37c7ec25a515480bbf3f21f
Cazaks <calebezak@gmail.com>
2025-08-16 03:48:02 +0100

    first push in Java Script

---
+++ JavaScript/arraySumming.js
@@ -0,0 +1,10 @@
+
+		let grades = [87, 68, 94, 100, 83, 78, 85, 91, 76, 87];
+		let total = 0;
+
+		for(let count = 0; count < grades.length; count++){
+			total += grades[count];
+		}
+
+		console.log(`The total grade score is ${total}`);
+	
